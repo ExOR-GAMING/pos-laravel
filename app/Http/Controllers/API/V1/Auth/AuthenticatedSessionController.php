30ebb0819a3e0feed144d20673f1140238c05238
ExOR_GAMING <fajarmi521@gmail.com>
2024-10-06 13:19:20 +0700

    Revoke old token on each login attempt

---
+++ app/Http/Controllers/API/V1/Auth/AuthenticatedSessionController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers\API\V1\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use App\Http\Resources\V1\User as UserResource;
+use App\Models\User;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules;
+
+
+class AuthenticatedSessionController extends Controller
+{
+    /**
+     * Handle an incoming login request.
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function store(LoginRequest $request) : JsonResponse
+    {
+        $request->authenticate();
+
+        $user = $request->user();
+        
+        $user->tokens->each(function ($value){
+            if (in_array('basic:full-access',$value->abilities)){
+                $value->delete();
+            }
+        });
+
+        $token = $user->createToken($request->device_name, ['basic:full-access'])->plainTextToken;
+
+        return response()->json([
+            'token' => $token,
+        ], 201);
+    }
+
+    /**
+     * Handle an incoming logout request.
+     */
+    public function destroy(Request $request): JsonResponse
+    {
+        $isDeletionSuccess = $request->user()->currentAccessToken()->delete();
+
+        if(!$isDeletionSuccess){
+            return response()->json(['message' => 'Error when logging out user'], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        return response()->json([], 204);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\V1\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\API\V1\Auth\RegisteredUserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -10,3 +11,9 @@ Route::get('/v1/user', function (Request $request) {
 
 Route::post('/v1/register', [RegisteredUserController::class, 'store'])
     ->middleware('guest');
+
+Route::post('/v1/login', [AuthenticatedSessionController::class, 'store'])
+    ->middleware('guest');
+    
+Route::post('/v1/logout', [AuthenticatedSessionController::class, 'destroy'])
+    ->middleware('auth:sanctum');
